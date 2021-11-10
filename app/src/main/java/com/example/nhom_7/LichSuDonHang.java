7bc9cbf178b2adcd8048bd860d365864853d27cd
leducphuoc2001 <leducphuoc.tdc2019@gmail.com>
2021-11-10 19:05:20 +0700

    Giao diện màn hình lịch sử đơn hàng

---
+++ app/src/main/java/com/example/nhom_7/LichSuDonHang.java
@@ -0,0 +1,115 @@
+package com.example.nhom_7;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nhom_7.adapter.LichSuDonHangAdapter;
+import com.example.nhom_7.model.LichSuDH;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
+public class LichSuDonHang extends AppCompatActivity {
+    RecyclerView lvLichSuDH;
+    ArrayList<LichSuDH> data = new ArrayList<LichSuDH>();
+    LichSuDonHangAdapter myRecyclerViewAdapter;
+    DatabaseReference database;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lich_su_don_hang);
+        setConTrol();
+        setEvent();
+        //list();
+    }
+        private void list(){
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("LichSuDonHang");
+        for (int i=1;i<5;i++){
+            String maDH= mDatabase.push().getKey();
+            LichSuDH hoaDon = new LichSuDH();
+            hoaDon.setMaDH(maDH);
+            hoaDon.setNgayDat("2"+i+"/01/2021");
+            hoaDon.setTong(50000*i);
+            hoaDon.setTrangThai("Chờ");
+            mDatabase.child(maDH).setValue(hoaDon);
+        }
+    }
+    //Lấy dữ liệu firebase
+    private void getlist(){
+        database= FirebaseDatabase.getInstance().getReference("LichSuDonHang");
+        database.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                LichSuDH donHang = snapshot.getValue(LichSuDH.class);
+                if(donHang != null){
+                    data.add(0,donHang);
+                    myRecyclerViewAdapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+                LichSuDH donHang = snapshot.getValue(LichSuDH.class);
+                if(donHang == null || data==null||data.isEmpty()){
+                    return;
+                }
+                for (int i = 0;i<data.size();i++){
+                    if(donHang.getMaDH()==data.get(i).getMaDH()){
+                        data.set(i,donHang);
+                    }
+                }
+                myRecyclerViewAdapter.notifyDataSetChanged();
+            }
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot snapshot) {
+
+            }
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot snapshot, @Nullable String previousChildName) {
+
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+    }
+    private void setEvent() {
+        myRecyclerViewAdapter = new LichSuDonHangAdapter(this,R.layout.layout_item_lich_su_don_hang,data);
+        myRecyclerViewAdapter.setDelegation(new LichSuDonHangAdapter.MyItemClickListener() {
+            @Override
+            public void getXacNhanDonHang(LichSuDH lichSuDH) {
+                database=FirebaseDatabase.getInstance().getReference("LichSuDonHang");
+                String trangThai = "Xác nhận";
+                lichSuDH.setTrangThai(trangThai);
+                database.child(String.valueOf(lichSuDH.getMaDH())).updateChildren(lichSuDH.toMap());
+            }
+
+            @Override
+            public void getHuyDonHang(LichSuDH lichSuDH) {
+                database=FirebaseDatabase.getInstance().getReference("LichSuDonHang");
+                String trangThai = "Huỷ";
+                lichSuDH.setTrangThai(trangThai);
+                database.child(String.valueOf(lichSuDH.getMaDH())).updateChildren(lichSuDH.toMap());
+            }
+        });
+        lvLichSuDH.setAdapter(myRecyclerViewAdapter);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(RecyclerView.VERTICAL);
+        lvLichSuDH.setLayoutManager(layoutManager);
+        getlist();
+    }
+
+    private void setConTrol() {
+        lvLichSuDH=findViewById(R.id.lvLichSuDH);
+    }
+}
---
+++ app/src/main/java/com/example/nhom_7/adapter/LichSuDonHangAdapter.java
@@ -0,0 +1,96 @@
+package com.example.nhom_7.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.nhom_7.R;
+import com.example.nhom_7.model.LichSuDH;
+
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+import java.util.ArrayList;
+
+public class LichSuDonHangAdapter extends RecyclerView.Adapter<LichSuDonHangAdapter.MyViewHolder>{
+    private Activity context;
+    private int layoutID;
+    private ArrayList<LichSuDH> donHangArrayList;
+    private LichSuDonHangAdapter.MyItemClickListener delegation;
+
+    public void setDelegation(LichSuDonHangAdapter.MyItemClickListener delegation) {
+        this.delegation = delegation;
+    }
+
+    public LichSuDonHangAdapter(Activity context, int layoutID, ArrayList<LichSuDH> donHangArrayList){
+        this.context= context;
+        this.layoutID=layoutID;
+        this.donHangArrayList=donHangArrayList;
+    }
+    @NonNull
+    @Override
+    public LichSuDonHangAdapter.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = context.getLayoutInflater();
+        CardView view =(CardView) layoutInflater.inflate(viewType, parent, false);
+        return new LichSuDonHangAdapter.MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull LichSuDonHangAdapter.MyViewHolder holder, int position) {
+        LichSuDH lichSuDH = donHangArrayList.get(position);
+        if(lichSuDH==null){
+            return;
+        }
+        holder.tvMa.setText(lichSuDH.getMaDH());
+        NumberFormat formatter = new DecimalFormat("#,###,###");
+        holder.tvTong.setText(formatter.format(lichSuDH.getTong()) + " đ");
+        holder.tvNgayDat.setText(lichSuDH.getNgayDat());
+        holder.tvTrangThai.setText(lichSuDH.getTrangThai());
+
+        holder.btnDaNhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delegation.getXacNhanDonHang(lichSuDH);
+            }
+        });
+        holder.btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                delegation.getHuyDonHang(lichSuDH);
+            }
+        });
+    }
+    @Override
+    public int getItemViewType(int position) {
+        return layoutID;
+    }
+    @Override
+    public int getItemCount() {
+        return donHangArrayList.size();
+    }
+
+    public static class  MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tvMa,tvNgayDat,tvTrangThai,tvTong;
+        Button btnHuy,btnDaNhan;
+
+        public MyViewHolder(@NonNull View itemView){
+            super(itemView);
+            tvMa = itemView.findViewById(R.id.tvMa);
+            tvTong = itemView.findViewById(R.id.tvTong);
+            tvNgayDat = itemView.findViewById(R.id.tvNgayDat);
+            tvTrangThai=itemView.findViewById(R.id.tvTrangthai);
+            btnDaNhan=itemView.findViewById(R.id.btnDaNhan);
+            btnHuy=itemView.findViewById(R.id.btnHuy);
+        }
+    }
+    public interface MyItemClickListener{
+        void getXacNhanDonHang(LichSuDH lichSuDH);
+        void getHuyDonHang(LichSuDH lichSuDH);
+    }
+}
